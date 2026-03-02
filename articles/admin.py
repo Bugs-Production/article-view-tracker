e7bb76c2d751034499a86e41b136a93a92dc6ab9
Vlad <belouskovlad@gmail.com>
2026-03-02 16:28:25 +0600

    add admin
    Signed-off-by: Vlad <belouskovlad@gmail.com>

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from articles.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "user_id", "is_published", "views_counter", "created_at"]
+    list_filter = ["is_published"]
+    search_fields = ["title", "content"]
+    list_editable = ["is_published"]
+    readonly_fields = ["views_counter", "created_at", "updated_at"]
